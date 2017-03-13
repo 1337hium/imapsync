32c0bbbf6d26887ed6c3592b5243639d82e8fd2b
root <root@1337hium.ch>
2017-03-13 15:39:08 +0100

    Files

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>IMAPSYNC</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../whois/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../rezept/style.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../rezept/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+   <![endif]-->
+    <style>
+
+      .left    { text-align:left; }
+      .right   { text-align: right; }
+      .center  { text-align: center; }
+      .justify { text-align: justify; }
+      .start   { text-align: start; }
+
+    </style>
+</head>
+<h1 class="center"> IMAPSYNC</h1>
+<html>
+<form action="sync.php" method="get">
+      <div class="container">
+      <div class="row"> 
+      <div class="col-md-4">
+
+  Alter Host: <input type="text" name="oldhost" placeholder="phoenix.webstyle.ch"><br>
+  Alte Mailadresse: <input type="text" name="olduser" placeholder="user@domain.tld"><br>
+  Altes Kennwort: <input type="password" name="oldpw"><br>
+  Alter Authmech: <input type="text" name="oldauth" value="PLAIN"><br>
+</div>
+<div class="row">
+      <div class="col-md-4">
+  Neuer Host: <input type="text" name="newhost" placeholder="phoenix.webstyle.ch"><br>
+  Neue Mailadresse: <input type="text" name="newuser" placeholder="user@domain.tld"><br> 
+  Neues Kennwort: <input type="password" name="newpw"><br>
+  Neuer Authmech: <input type="text" name="newauth" value="PLAIN"><br> 
+<button type="submit" class="btn btn-default">Submit</button>
+</form>
+</div>
+<div class="row">
+      <div class="col-md-4">
+<br>
+Hostname eingeben<br>
+<br>
+<br>
+<br>
+E-Mail Adresse
+<br>
+<br>
+<br>
+<br>
+Kennwörter von altem und neuem Server
+<br>
+<br>
+<br>
+<br>
+PLAIN oder LOGIN, wobei PLAIN zu 99% funktioniert
---
+++ sync.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>WHOIS</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../whois/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../rezept/style.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../rezept/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+   <![endif]-->
+    <style>
+
+      .left    { text-align:left; }
+      .right   { text-align: right; }
+      .center  { text-align: center; }
+      .justify { text-align: justify; }
+      .start   { text-align: start; }
+
+    </style>
+</head>
+<body>
+<script>
+function Scrollit(){ window.scrollBy(0,1); }
+setInterval("Scrollit()",0.1);
+</script>
+<h1 class="center"> IMAPSYNC</h1>
+<html>
+</div>
+<div class="row">   
+      <div class="col-md-4">
+</div>
+</div>
+<?php
+header('Content-Encoding: none;');
+
+
+$oldhost = $_GET["oldhost"];
+$olduser = $_GET["olduser"];
+$oldpw = $_GET["oldpw"];
+$oldauth = $_GET["oldauth"];
+$newhost = $_GET["newhost"];
+$newuser = $_GET["newuser"];
+$newpw = $_GET["newpw"];
+$newauth = $_GET["newauth"];
+
+echo '<pre>';
+$output = system("imapsync --host1 '$oldhost' --user1 '$olduser' --password1 '$oldpw' --authmech1 '$oldauth' --host2 '$newhost' --user2 '$newuser' --password2 '$newpw' --authmech2 '$newauth' --ssl2");
+echo '</pre>';
+
+
+?>
+</br>
+</br>
+
+<button type="submit" name="update" id="update" class="btn btn-default" onclick="goBack()">zurück</button>
+
+<script>
+function goBack() {
+    window.history.back();
+}
+</script>
